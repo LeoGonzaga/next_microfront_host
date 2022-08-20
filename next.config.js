12075402ad308fff11ce97d21572950da351283b
Leo Gonzaga <sacredlotus05@gmail.com>
2022-08-20 01:32:08 -0300

    feat: chaning filename

--- next.config.js
+++ next.config.js
@@ -1,42 +1,44 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
+  future: {
+    webpack5: true,
+  },
 
-
-  
   webpack: (config, options) => {
-    config.experiments = { topLevelAwait: true };
-    config.output.publicPath = "http://localhost:3001/_next/";
+    config.output.publicPath = "http://localhost:3000/_next/";
+    console.log(config)
     config.plugins.push(
       new options.webpack.container.ModuleFederationPlugin({
-        name:"mf3",
+        name: "fe3",
+        filename: "static/chunks/remoteEntry.js",
         remoteType: "var",
-        
         remotes: {
-            mf1: 'mf1'
+          mf1: "mf1",
         },
-       
+
         shared: [
           {
             react: {
               eager: true,
               singleton: true,
               requiredVersion: false,
-            }
+            },
           },
           {
             "react-dom": {
               eager: true,
               singleton: true,
               requiredVersion: false,
-            }
+            },
           },
-        ]
+        ],
       })
-    )
-    return config
-  }
-}
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,27 +2,27 @@ import dynamic from 'next/dynamic';
 
 
 const SampleComponent = dynamic(() => import("mf1/sidebar"), {
-    ssr: false,
-    loading({ error }) {
-      if (error) {
-        console.log(error)
-        return <p>{JSON.stringify(error)}</p>;
-      }
-  
-      return <p>Loading...</p>;
+  ssr: false,
+  loading({ error }) {
+    if (error) {
+      console.log(error)
+      return <p>{JSON.stringify(error)}</p>;
     }
-  });
+
+    return <p>Loading...</p>;
+  }
+});
 
 
 export default function Home() {
-    return (
-        <div>
-            <SampleComponent/>
-            <main >
-                <h1 >
-                    HOST
-                </h1>
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <SampleComponent />
+      <main >
+        <h1 >
+          HOST
+        </h1>
+      </main>
+    </div>
+  )
 }
